5facda5580e66fbcae1fb4156112f1bbc3511146
sameersayani <sameersayani@gmail.com>
2025-10-02 03:38:13 +0530

    allow cors for invoice frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://invoice-frontend-7ktd.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
