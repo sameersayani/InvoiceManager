898a69e8396b29d4d8c48a944aff671e39e5d040
sameersayani <sameersayani@gmail.com>
2025-10-05 01:02:15 +0530

    enable cors for yesitech and subdomain invygo

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://invoice-frontend-7ktd.onrender.com"],
+    allow_origins=["http://localhost:5173", 
+                   "https://invoice-frontend-7ktd.onrender.com",
+                   "https://yesitech.com",
+                   "https://invygo.yesitech.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
