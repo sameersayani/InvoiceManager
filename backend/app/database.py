cee0dd5a7e1aabbf0dced788536ee6a102685107
sameersayani <sameersayani@gmail.com>
2025-11-01 12:03:28 +0530

    connection update

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,13 +5,16 @@ import os
 
 # DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:sameer@localhost:5432/invoice_db")
 # DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://sameer:y65R63viWRvZMgPmmm5x4UOHwhGhfBCL@dpg-d3e36vndiees73fr0q2g-a.oregon-postgres.render.com/invoice_db_uots")
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://sameer:6BxDIvvmXdTzk1oV29oUekgjHTXpg3Ed@dpg-d42pio1r0fns739is3dg-a.oregon-postgres.render.com/invoice_db_jrr1?sslmode=require")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://sameer:6BxDIvvmXdTzk1oV29oUekgjHTXpg3Ed@dpg-d42pio1r0fns739is3dg-a.oregon-postgres.render.com/invoice_db_jrr1")
 
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True,       # keeps connections fresh
+    connect_args={
+        "sslmode": "require"
+    },
+    pool_pre_ping=True,
+    pool_recycle=1800
 )
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
