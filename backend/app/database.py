7604cc6ae57552264f3181941b40bed833321cd0
sameersayani <sameersayani@gmail.com>
2025-11-01 11:36:31 +0530

    new db instance connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 # DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:sameer@localhost:5432/invoice_db")
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://sameer:y65R63viWRvZMgPmmm5x4UOHwhGhfBCL@dpg-d3e36vndiees73fr0q2g-a.oregon-postgres.render.com/invoice_db_uots")
+# DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://sameer:y65R63viWRvZMgPmmm5x4UOHwhGhfBCL@dpg-d3e36vndiees73fr0q2g-a.oregon-postgres.render.com/invoice_db_uots")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://sameer:6BxDIvvmXdTzk1oV29oUekgjHTXpg3Ed@dpg-d42pio1r0fns739is3dg-a.oregon-postgres.render.com/invoice_db_jrr1")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
