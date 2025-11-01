c38223ed6c644fcf250ef2ab5cd9262f0db8c7cb
sameersayani <sameersayani@gmail.com>
2025-11-01 11:49:52 +0530

    keep connection fresh

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,7 +7,11 @@ import os
 # DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://sameer:y65R63viWRvZMgPmmm5x4UOHwhGhfBCL@dpg-d3e36vndiees73fr0q2g-a.oregon-postgres.render.com/invoice_db_uots")
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://sameer:6BxDIvvmXdTzk1oV29oUekgjHTXpg3Ed@dpg-d42pio1r0fns739is3dg-a.oregon-postgres.render.com/invoice_db_jrr1?sslmode=require")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,       # keeps connections fresh
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
