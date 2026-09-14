c50e21c3603f6726e5ded59c81a8a3cba3db336a
sameersayani <sameersayani@gmail.com>
2026-09-14 23:15:05 +0530

    fixes for url redirects

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // or '@vitejs/plugin-vue' depending on your framework
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This forces absolute routing paths instead of relative ones
+  base: '/', // This forces absolute routing asset imports
 })
